cf04b80863d6a6cc048a281e334bc1e6fbf36d8e
Lehmniscate <mchlhtn@gmail.com>
2017-03-14 23:37:12 -0700

    added basic back end session control

--- config/routes.rb
+++ config/routes.rb
@@ -2,4 +2,9 @@ Rails.application.routes.draw do
 
   root to: 'static_pages#root'
 
+  namespace :api, defaults: {format: :json} do
+    resource :session, only: [:create, :destroy]
+    resources :users, only: [:index, :show, :create, :destroy, :update]
+  end
+
 end
